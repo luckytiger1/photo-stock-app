8de3ca39fd2f8243c93ede9dc1fc3abc149b3e80
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-04-03 01:04:14 +0600

    feat: add error-boundary component

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import ErrorIndicator from '../ErrorIndicator/ErrorIndicator';
+
+type MyState = {
+  hasError: boolean;
+};
+
+type ErrorBoundaryProps = {
+  children: React.ReactNode;
+};
+
+export default class ErrorBoundary extends React.Component<{}, MyState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+    if (hasError) {
+      return <ErrorIndicator />;
+    }
+    return children;
+  }
+}
